f124bf5e32b35a39eb3762ffc5ec7a0d30c53dc2
Gavin Roberts <gavinsroberts13@gmail.com>
2021-10-14 20:40:30 -0700

    Added file drop down menu
    Added file drop down menu

--- src/phreedf/helper_func.py
+++ src/phreedf/helper_func.py
@@ -2,28 +2,5 @@ import os
 import math
 from PIL import Image
 from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfFileWriter
 
-def extract_information(pdf_path):
-    with open(pdf_path, 'rb') as f:
-        pdf = PdfFileReader(f)
-        # pdf.decrypt("")
-        information = pdf.getDocumentInfo()
-        number_of_pages = pdf.getNumPages()
-
-    txt = f"""
-    Information about {pdf_path}: 
-
-    Author: {information.author}
-    Creator: {information.creator}
-    Producer: {information.producer}
-    Subject: {information.subject}
-    Title: {information.title}
-    Number of pages: {number_of_pages}
-    """
-    
-    print(txt)
-    return information
-
-if __name__ == '__main__':
-    path = os.path.join(os.path.split(os.path.abspath(__file__))[0], '../data/pdf-test-2.pdf')
-    extract_information(path)
--- src/phreedf/phreedf.py
+++ src/phreedf/phreedf.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 from PIL import ImageTk, Image
+from tkPDFViewer import tkPDFViewer as pdf
+import PyPDF2 
 
 import os
 from menubar import Menubar
@@ -11,29 +14,59 @@ WIDTH = 1920
 class PhreeDFApp():
     
     def __init__(self, root):
-        
         pathToAppIcon = os.path.join(os.path.split(os.path.abspath(__file__))[0], '../icon/icon.png')
         img = Image.open(pathToAppIcon)
         icon = ImageTk.PhotoImage(img)
+
         root.tk.call('wm', 'iconphoto', root._w, icon)
         root.geometry( str(HEIGHT) + "x" + str(WIDTH) )
 
         root.title("PhreeDF")
 
         ribbon = ttk.Notebook(root)
+        text= tk.Text(ribbon,width= 80,height=30)
+        text.pack(pady=20)
         ribbon.place(relheight=1, relwidth=1)
         
-
         self.menuBar = Menubar(ribbon)
         ribbon.select(self.menuBar.homeFrame)
 
-        root.bind('<Escape>', close)
+        my_menu= tk.Menu(root)
+        root.config(menu=my_menu)
 
+        file_menu=tk.Menu(my_menu,tearoff=False)
+        my_menu.add_cascade(label="File",menu= file_menu)
+        file_menu.add_command(label="Open",command=self.open_pdf)
+        file_menu.add_command(label="Close",command=self.close)
     
-def close(event):
-    root.destroy()
+
+        
+
+        root.bind('<Escape>', self.close)
+    
+    def clear_text(self):
+        self.text.delete(1.0, tk.END)
+    
+    def open_pdf(self):
+        file = filedialog.askopenfilename(title="Select a PDF", filetype=(("PDF    Files","*.pdf"),("All Files","*.*")))
+        if file:
+            #Open the PDF File
+            pdf_file= PyPDF2.PdfFileReader(file)
+            #Select a Page to read
+            page= pdf_file.getPage(0)
+            #Get the content of the Page
+            content=page.extractText()
+            print(content)
+            #Add the content to TextBox
+            self.text.insert(1.0,content)
+
+    def close(self,event=-1):
+        root.destroy()
+        print("Goodbye")
+
+
     
 if __name__ == "__main__":
     root = tk.Tk()
-    PhreeDFApp(root)
+    mainApp = PhreeDFApp(root)
     root.mainloop()
